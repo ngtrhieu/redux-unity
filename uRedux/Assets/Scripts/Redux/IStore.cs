c5b126f90eb83eab0cb9ea66987120e8af24a995
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-25 17:45:49 +0800

    docs(redux): document Store API

--- uRedux/Assets/Scripts/Redux/IStore.cs
+++ uRedux/Assets/Scripts/Redux/IStore.cs
@@ -2,23 +2,9 @@
 
 namespace uRedux {
   public interface IStore<TState> {
-    /// <summary>
-    /// Dispatch an action to the store.
-    /// </summary>
-    /// <param name="action">
-    /// The action to dispatch.
-    /// </param>
-    /// <returns>
-    /// Depends on existing store middlewares. With no middlewares, returns the same action that was passed into.
-    /// </returns>
+
     IAction Dispatch(IAction action);
 
-    /// <summary>
-    /// Get the current state.
-    /// </summary>
-    /// <return>
-    /// The current state tree.
-    /// <returns>
     TState GetState();
 
     event Action StageChanged;
--- uRedux/Assets/Scripts/Redux/Store.cs
+++ uRedux/Assets/Scripts/Redux/Store.cs
@@ -4,9 +4,8 @@ using System;
 
 namespace uRedux {
   public class Store<TState> : IStore<TState> {
-    public event Action StageChanged;
-
-    private readonly ReducerDelegate<TState> reducer;
+    private readonly object syncLock = new object();
+    private ReducerDelegate<TState> reducer;
     private readonly DispatcherDelgate dispatcher;
     private TState state;
 
@@ -16,30 +15,52 @@ namespace uRedux {
       this.state = initialState;
     }
 
+    /// <summary>
+    /// Replaces the reducer currently used by the store to calculate the state.
+    /// </summary>
+    /// <remarks>
+    /// This might be useful for implementing hot reloading, dynamically built reducers, and/or alternating the behaviours of reducers.
+    /// </remarks>
+    public void ReplaceReducer(ReducerDelegate<TState> reducer) {
+      this.reducer = reducer;
+    }
+
+    /// <summary>
+    /// Dispatches an action. This is the only way to trigger a state change.
+    /// </summary>
+    /// <remarks>
+    /// The reducer will be triggered with this action. The returned value will be considered the **next** state.
+    /// Subscribers to `StagedChanged` event will be notified.
+    /// </remarks>
+    /// <returns>Depends on the existing middlewares. With no middlewares, the same action passed in will be returned.</returns>
     public IAction Dispatch(IAction action) {
       return this.dispatcher(action);
     }
 
+    /// <summary>
+    /// Read the state tree managed by the store.
+    /// </summary>
+    /// <returns>The current state tree of your application.</returns>
     public TState GetState() {
       return state;
     }
 
     private DispatcherDelgate ApplyMiddlewares(params MiddlewareDelegate<TState>[] middlewares) {
-      DispatcherDelgate dispatcher = InnerDispatcher;
+      DispatcherDelgate dispatcher = InitialDispatcher;
       foreach (var middleware in middlewares) {
         dispatcher = middleware(this)(dispatcher);
       }
       return dispatcher;
     }
 
-    private readonly object syncLock = new object();
-
-    private IAction InnerDispatcher(IAction action) {
+    private IAction InitialDispatcher(IAction action) {
       lock (syncLock) {
         state = reducer(state, action);
       }
       StageChanged?.Invoke();
       return action;
     }
+
+    public event Action StageChanged;
   }
 }
