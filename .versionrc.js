81706890a3b396689d9aa98fa2a5680db7bc851a
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-30 12:22:20 +0800

    chore: fix yarn release does not commit CHANGELOG to package folder

--- .versionrc.js
+++ .versionrc.js
@@ -12,5 +12,7 @@ module.exports = {
   scripts: {
     postchangelog:
       "cp CHANGELOG.md ./uRedux/CHANGELOG.md && cp README.md ./uRedux/README.md && cp LICENSE ./uRedux/LICENSE",
+    postcommit:
+      "git commit ./uRedux/CHANGELOG.md ./uRedux/README.md ./uRedux/LICENSE --amend --no-edit --no-verify",
   },
 };
